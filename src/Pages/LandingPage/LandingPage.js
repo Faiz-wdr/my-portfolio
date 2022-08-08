6fdda58843554b9b1bcde345a1f6342d23fcab27
FaizWdr <fayiz.kappil@gmail.com>
2022-08-08 14:57:26 +0530

    Responsive First Commit

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,18 +1,18 @@
-import React, { } from 'react'
-import {  } from 'react-router-dom'
-import NavBar from '../../components/NavBar/NavBar';
-import About from '../../components/Aboutme/Aboutme';
-import Footer from '../../components/Footer/Footer';
-import ProjectCard from '../../components/ProjectCard/ProjectCard';
-import './LandingPage.css'
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import Bgimg from '../../assets/img/Bgimg.png'
+import React from "react";
+import {} from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar";
+import About from "../../components/Aboutme/Aboutme";
+import Footer from "../../components/Footer/Footer";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import "./LandingPage.css";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import vector from "../../assets/img/3dimg.png";
 
-function LandingPage(){
-
-    return (
-      <div className="Body">
-        <div className="Banner">
+function LandingPage() {
+  return (
+    <div className="Body">
+      <div className="Banner">
+        <div className="flex">
           <div className="Heading">
             <h1>
               Hi! I'm <span>Faiz</span>
@@ -22,14 +22,25 @@ function LandingPage(){
               <br />
               Web Developer
             </p>
-            <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-          </div>
+            <div className="btn">
+          <button onClick={() => console.log("connect")}>
+            Let’s Connect <ArrowRightCircle size={25} />
+          </button>
         </div>
-        <NavBar />
-        <About />
-        <ProjectCard />
-        <Footer />
+          </div>
+            <div className="img">
+              <img size="small" alt="vector img" src={vector}></img>
+            </div>
+        </div>
+        
       </div>
-    );
-} 
-export default LandingPage
+      <div className="navbar">
+        <NavBar />
+      </div>
+      <About />
+      <ProjectCard />
+      <Footer />
+    </div>
+  );
+}
+export default LandingPage;
