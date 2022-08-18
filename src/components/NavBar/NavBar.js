ff6a4606ee0e852054ff8d8b5032000f97982eb8
FaizWdr <fayiz.kappil@gmail.com>
2022-08-18 16:53:10 +0530

    AnchorLink Added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import {React, useRef} from 'react'
+import AnchorLink from "react-anchor-link-smooth-scroll";
 // import { useNavigate } from 'react-router-dom'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import './NavBar.css'
@@ -16,11 +17,11 @@ function NavBar() {
         <header>
             <nav ref={navRef} className='nav_bar' >
                 <div className='nav-text'>
-                    <a href="/#">Home</a>
-                    <a href="/#">About</a>
-                    <a href="/#">Skills</a>
-                    <a href="/#">Projects</a>
-                    <a href="/#">Blogs</a>
+                    <AnchorLink href='#Home'><a href="/#">Home</a></AnchorLink>
+                    <AnchorLink href='#About'><a href="/#">About</a></AnchorLink>
+                    <AnchorLink href='#Skills'><a href="/#">Skills</a></AnchorLink>
+                    <AnchorLink href='#Projects'><a href="/#">Projects</a></AnchorLink>
+                    <AnchorLink href='#Home'><a href="/#">Blogs</a></AnchorLink>
                 </div>
                 <button className='nav-btn nav-close-btn' onClick={showNavBar}>
                     <FaTimes />
